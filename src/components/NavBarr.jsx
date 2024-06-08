44bab8f6f751b36b50c5e20bafead88380e1dbce
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-06-08 22:42:05 +0530

    error fixed, will make some changes later on

--- src/components/NavBarr.jsx
+++ src/components/NavBarr.jsx
@@ -3,38 +3,6 @@ import gsap from 'gsap';
 import { CiShoppingCart } from "react-icons/ci";
 
 function NavBarr() {
-  const navRef = useRef(null);
-  let lastScrollY = 0;
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      lastScrollY = window.scrollY;
-      
-      const handleScroll = () => {
-        const currentScrollY = window.scrollY;
-        if (currentScrollY > lastScrollY) {
-          // User is scrolling down, hide the navbar
-          gsap.to(navRef.current, { y: '-100%', opacity: 0, duration: 0.5 });
-        } else {
-          // User is scrolling up, show the navbar
-          gsap.to(navRef.current, { y: '0%', opacity: 1, duration: 0.5 });
-        }
-        lastScrollY = currentScrollY;
-      };
-
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }
-  }, []);
-
-  const scrollToSection = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
 
   return (
     <div ref={navRef} className="sticky bg-gray-950 bg-opacity-80 top-0 px-2 py-6 bg-transparent md:px-2 flex justify-evenly">
@@ -42,9 +10,9 @@ function NavBarr() {
         <ul><li className="font-bold">Threads Clothing</li></ul>
       </div>
       <ul className="flex gap-5 font-bold">
-        <li className="cursor-pointer" onClick={() => scrollToSection('homePage')}>Home</li>
-        <li className="cursor-pointer" onClick={() => scrollToSection('collections')}>Collections</li>
-        <li className="cursor-pointer" onClick={() => scrollToSection('aboutUs')}>About Us</li>
+        <li className="cursor-pointer">Home</li>
+        <li className="cursor-pointer">Collections</li>
+        <li className="cursor-pointer">About Us</li>
       </ul>
       <ul className="flex gap-1 font-bold">
         <li>Cart</li>
