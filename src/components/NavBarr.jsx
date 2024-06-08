66170516b8c25f3e49eecfdd4a3d798e57a075a9
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-06-08 22:46:03 +0530

    bug fixed #1

--- src/components/NavBarr.jsx
+++ src/components/NavBarr.jsx
@@ -4,15 +4,34 @@ import { CiShoppingCart } from "react-icons/ci";
 
 function NavBarr() {
 
+  const scrollToAboutUs = () => {
+    const aboutUsSection = document.getElementById("aboutUs");
+    if (aboutUsSection) {
+      aboutUsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const scrollToCollections = () => {
+    const contactUsSection = document.getElementById("collections");
+    if (contactUsSection) {
+      contactUsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  const scrollToHomePage = () => {
+    const homePageSection = document.getElementById("homePage");
+    if (homePageSection) {
+      homePageSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="sticky bg-gray-950 bg-opacity-80 top-0 px-2 py-6 bg-transparent md:px-2 flex justify-evenly">
       <div className="flex items-center justify-center">
         <ul><li className="font-bold">Threads Clothing</li></ul>
       </div>
       <ul className="flex gap-5 font-bold">
-        <li className="cursor-pointer">Home</li>
-        <li className="cursor-pointer">Collections</li>
-        <li className="cursor-pointer">About Us</li>
+        <li className="cursor-pointer" onClick={scrollToHomePage}>Home</li>
+        <li className="cursor-pointer" onClick={scrollToCollections}>Collections</li>
+        <li className="cursor-pointer" onClick={scrollToAboutUs}>About Us</li>
       </ul>
       <ul className="flex gap-1 font-bold">
         <li>Cart</li>
